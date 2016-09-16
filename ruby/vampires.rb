76538ac16d4f4ef0fce9255d5c8ab76e121d566d
cheleylin <linqiaoshu1992@gmail.com>
2016-09-16 16:36:41 -0700

    Created vampire questions and applied control flow to test for vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,51 @@
+result = "Results inconclusive."
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year_of_birth = gets.chomp.to_i
+
+if age == (2016 - year_of_birth)
+  age_test = true
+else
+  age_test = false
+end
+
+puts "Do you like garlic bread? (y/n)"
+garlic = gets.chomp
+
+if garlic == "y"
+  garlic_test = true
+else
+  garlic_test = false
+end
+
+puts "Are you interested in health insurance? (y/n)"
+insurance = gets.chomp
+
+if insurance == "y"
+  insurance_test = true
+else
+  insurance_test = false
+end
+
+if name == "Drake Cula" || name == "Tu Fang"
+  result = "Definitely a vampire."
+elsif !age_test
+  if !garlic_test && !insurance_test
+    result = "Almost certainly a vampire."
+  elsif !garlic_test || !insurance_test
+    result = "Probably not a vampire"
+  end
+else
+  if garlic_test || insurance_test
+    result = "Probably not a vampire."
+  end
+end
+
+puts "#{result}"
+  
