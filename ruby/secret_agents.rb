b5391179b9cb9bb144cd4a19176694685753098e
cheleylin <linqiaoshu1992@gmail.com>
2016-09-16 19:35:20 -0700

    Add interface to program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,18 +1,18 @@
 #encrypt method
-#create a result string and set to empty
-#for loop every char in string
-# => if the char is a space
-# =>    attach space to result string
-# => else use .next to increment then store in result
 
 def encrypt(str)
+  #create a result string and set to empty
   result = ""
+  #for loop every char in string
   for i in 0...str.length
     char = str[i]
+    #check if char is space
     if char == " "
       result += " "
+    #check if char is "z"
     elsif char == "z"
       result += "a"
+    #use .next to increment then store in result
     else
       result += char.next
     end
@@ -22,18 +22,17 @@ def encrypt(str)
 end
 
 #decrypt method
-#create an empty result string
-#use for loop to go through every char in string
-# => check for space
-# => locate the char's index in codex
-# => attach previous char to result
 def decrypt(str)
   decoder_ring = "abcdefghijklmnopqrstuvwxyz"
+  #create an empty result string
   result = ""
+  #use for loop to go through every char in string
   for i in 0...str.length
     char = str[i]
+    #check for space
     if char == " "
       result += " "
+    #locate the previous char and attach it to result
     else
       result += decoder_ring[decoder_ring.index(char)-1]
     end
@@ -42,5 +41,32 @@ def decrypt(str)
   return result
 end
 
-puts "#{decrypt("bcd")}"
-puts "#{decrypt("afe")}"
+# puts "#{decrypt("bcd")}"
+# puts "#{decrypt(encrypt("swordfish"))}"
+
+#Interface
+#Ask user if they want to encrypt or decrypt
+puts "Would you like to encrypt or decrypt a message?"
+puts "encrypt = e / decrypt = d"
+option = gets.chomp
+
+#Ask them for password
+puts "Please enter the password: "
+password = gets.chomp
+
+#Conduct operation & print result
+if option == "e" || option == "E"
+  puts "The encrypted password is:"
+  puts encrypt(password)
+elsif option == "d" || option == "D"
+  puts "The decrypted password is:"
+  puts decrypt(password)
+else
+  puts "Please enter 'e' or 'd'."
+end
+
+
+
+
+
+
