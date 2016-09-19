1168021f2db76d3c49f031ae61f087c7cdf66635
cheleylin <linqiaoshu1992@gmail.com>
2016-09-19 00:02:10 -0700

    Add while loop to process multiple employees at once

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,51 +1,59 @@
-result = "Results inconclusive."
+puts "Please input how many employees you want to process:"
+number = gets.chomp.to_i
 
-puts "What is your name?"
-name = gets.chomp
+while number != 0
+  result = "Results inconclusive."
 
-puts "How old are you?"
-age = gets.chomp.to_i
+  puts "What is your name?"
+  name = gets.chomp
 
-puts "What year were you born?"
-year_of_birth = gets.chomp.to_i
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-if age == (2016 - year_of_birth)
-  age_test = true
-else
-  age_test = false
-end
+  puts "What year were you born?"
+  year_of_birth = gets.chomp.to_i
 
-puts "Do you like garlic bread? (y/n)"
-garlic = gets.chomp
-
-if garlic == "y"
-  garlic_test = true
-else
-  garlic_test = false
-end
-
-puts "Are you interested in health insurance? (y/n)"
-insurance = gets.chomp
-
-if insurance == "y"
-  insurance_test = true
-else
-  insurance_test = false
-end
-
-if name == "Drake Cula" || name == "Tu Fang"
-  result = "Definitely a vampire."
-elsif !age_test
-  if !garlic_test && !insurance_test
-    result = "Almost certainly a vampire."
-  elsif !garlic_test || !insurance_test
-    result = "Probably a vampire"
+  if age == (2016 - year_of_birth)
+    age_test = true
+  else
+    age_test = false
   end
-else
-  if garlic_test || insurance_test
-    result = "Probably not a vampire."
+
+  puts "Do you like garlic bread? (y/n)"
+  garlic = gets.chomp
+
+  if garlic == "y"
+    garlic_test = true
+  else
+    garlic_test = false
   end
+
+  puts "Are you interested in health insurance? (y/n)"
+  insurance = gets.chomp
+
+  if insurance == "y"
+    insurance_test = true
+  else
+    insurance_test = false
+  end
+
+  if name == "Drake Cula" || name == "Tu Fang"
+    result = "Definitely a vampire."
+  elsif !age_test
+    if !garlic_test && !insurance_test
+      result = "Almost certainly a vampire."
+    elsif !garlic_test || !insurance_test
+      result = "Probably a vampire"
+    end
+  else
+    if garlic_test || insurance_test
+      result = "Probably not a vampire."
+    end
+  end
+
+  puts "#{result}"
+  number -= 1
 end
 
-puts "#{result}"
+puts "All employees processed."
   
