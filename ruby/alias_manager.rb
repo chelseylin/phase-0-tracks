ba09c83790da0d8fe310a285e23f907b71becdf5
cheleylin <linqiaoshu1992@gmail.com>
2016-09-22 17:03:45 -0700

    Created code for generating fake name

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,36 @@
+#Input Real Name
+puts "Please Enter the Name You Wish to Change:"
+real_name = gets.chomp
+
+#Create Fake Name
+# => Swap First and Last Name
+word_array = real_name.split(' ')
+fake_name = word_array[1] + ' ' + word_array[0]
+char_array = fake_name.downcase.chars
+
+# => Change Vowels to Next
+vowel_ring = "aeiou"
+char_array.map! do |char|
+  index = vowel_ring.index(char)
+  if index != nil
+    vowel_ring[(index+1)%5]
+  else
+    char
+  end
+end
+
+# => Change Consonants to Next
+consonant_ring = "bcdfghjklmnpqrstvwxyz"
+char_array.map! do |char|
+  index = consonant_ring.index(char)
+  if index != nil
+    consonant_ring[(index+1)%21]
+  else
+    char
+  end
+end
+
+#Print Fake Name
+fake_name = char_array.join('')
+fake_name = fake_name.split(' ').map(&:capitalize).join(' ')
+puts fake_name
