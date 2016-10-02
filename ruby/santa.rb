b29a1ca24ff67fe62f8978f4419c2535ca04128e
cheleylin <linqiaoshu1992@gmail.com>
2016-10-02 16:01:50 -0700

    added attributes and tested multiple way of initializing

---
+++ ruby/santa.rb
@@ -0,0 +1,42 @@
+class Santa
+  #---Initialize Method---
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
+  #---Instance Methods---
+  def speak()
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+end
+
+# santa_test = Santa.new
+# santa_test.speak
+# santa_test.eat_milk_and_cookies("chocolate chip cookie")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+# p santas
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
