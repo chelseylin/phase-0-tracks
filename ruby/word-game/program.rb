cb1d882041181d2bd62722c97d06b05682438ceb
cheleylin <linqiaoshu1992@gmail.com>
2016-10-03 01:09:57 -0700

    Created brief pseudocode for game class

---
+++ ruby/word-game/program.rb
@@ -0,0 +1,10 @@
+#Hang-man Game
+#Create a class to store data and methods used by game
+# => attributes: if_win, answer, guess_num, letter_available, current
+# => initialize: if_win = false, method to get answer, 
+#                calculate guess_num, letter_avialbe = default alphabet
+#                current = blanks
+# => method: display -- prints current, guesses_left, letter_available
+#            ask for answer -- ask user to input a word for answer
+#            enter a letter -- takes in a letter and calculate all relating
+
