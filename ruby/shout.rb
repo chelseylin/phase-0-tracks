c24e442deae8e6e585391c9ab1c3215ac4858608
cheleylin <linqiaoshu1992@gmail.com>
2016-10-03 00:56:14 -0700

    Changed to mixin module and tested in classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yell_happily(words)
-    words + "!!!" + " :D"
-  end
-end
+#   def self.yell_happily(words)
+#     words + "!!!" + " :D"
+#   end
+# end
 
 # puts Shout.yell_happily("Yeah")
 # puts Shout.yell_angrily("Hey")
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words + "!!!" + " :D"
+  end
+
+end
+
+class Teacher
+  include Shout
+end
+
+class Boss
+  include Shout
+end
+
+teacher = Teacher.new
+puts teacher.yell_angrily("Stop cheating")
+boss = Boss.new
+puts boss.yell_happily("We made it")
