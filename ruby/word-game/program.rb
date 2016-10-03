a8bfd45e8c978f2304e4a60295a7c83376235ed8
cheleylin <linqiaoshu1992@gmail.com>
2016-10-03 01:57:15 -0700

    Created and tested codes for program

--- ruby/word-game/program.rb
+++ ruby/word-game/program.rb
@@ -1,10 +1,53 @@
 #Hang-man Game
 #Create a class to store data and methods used by game
 # => attributes: if_win, answer, guess_num, letter_available, current
-# => initialize: if_win = false, method to get answer, 
+# => initialize: if_win = false, answer 
 #                calculate guess_num, letter_avialbe = default alphabet
 #                current = blanks
 # => method: display -- prints current, guesses_left, letter_available
-#            ask for answer -- ask user to input a word for answer
-#            enter a letter -- takes in a letter and calculate all relating
+#            turn -- takes in a letter and calculate all relating
+#            check -- check winning status
 
+
+class Hangman
+  attr_reader :if_win
+  attr_reader :guess_num
+
+  def initialize(word)
+    @if_win = false
+    @answer = word
+    @guess_num = @answer.length
+    @current = []
+    @answer.length.times {@current << "_"}
+    @letter_available = "abcdefghijklmnopqrstuvwxyz"
+  end
+
+  def display
+    puts "Current Process: " + @current.join(" ")
+    puts "Letters Available: " + @letter_available
+    puts "Guesses Left: " + @guess_num.to_s
+  end
+
+  def turn(letter)
+    correct = false
+    if @letter_available.include?(letter)
+      index = 0
+      @answer.each_char do |char|
+        if char == letter
+          @current[index] = letter
+          correct = true
+        end
+        index += 1
+      end
+    end
+    @letter_available.delete!(letter)
+    @guess_num -= 1
+    return correct
+  end
+
+  def check
+    if @current.join == @answer
+      @if_win = true
+    end
+  end
+end
