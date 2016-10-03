4efa1805ca06c6e4e31efb861ad00402240a4f52
cheleylin <linqiaoshu1992@gmail.com>
2016-10-03 00:14:09 -0700

    added some instance method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(n)
+    n.times {puts "Woof!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_age)
+    dog_year = human_age / 7
+  end
+
+  def chasing_tail
+    puts "*chasing tail*"
+  end
+
 end
 
+test = Puppy.new
+test.fetch("ball")
+test.speak(5)
+test.roll_over
+puts test.dog_years(14)
+test.chasing_tail
