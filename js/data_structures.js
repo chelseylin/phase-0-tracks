04783dd080f2d63695baef44358be6d131016510
cheleylin <linqiaoshu1992@gmail.com>
2016-10-10 00:44:33 -0700

    Release 1: Created an array of color and an array of names, and practiced how to add item to array

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "yellow", "black"];
+var names = ["Abby", "Ed", "Bobby", "Vladimir"];
+
+// Add another color: pink
+colors.push("pink");
+// Add another name: Jack
+names.push("Jack");
+
