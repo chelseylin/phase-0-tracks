8de15c83df0ab90b8138bc9b8919c637790d6f10
cheleylin <linqiaoshu1992@gmail.com>
2016-10-10 01:54:35 -0700

    Release 2: Added and Tested RandomData function and fixed some undeclared variables

--- js/algos.js
+++ js/algos.js
@@ -10,7 +10,7 @@
 
 function findLongest(array) {
   var result = "";
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     if (array[i].length > result.length) {
       result = array[i];
     }
@@ -32,7 +32,7 @@ console.log(findLongest(["long phrase","longest phrase","longer phrase"]));
 
 function keyValueMatch(object1, object2) {
   var keys = Object.keys(object1);
-  for (i = 0; i < keys.length; i++) {
+  for (var i = 0; i < keys.length; i++) {
     var key = keys[i];
     if (object1[key] == object2[key]) {
       return true;
@@ -44,3 +44,38 @@ function keyValueMatch(object1, object2) {
 /* Driver Code */
 console.log(keyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
 
+/*****************************************************************
+ Release 2: Generate Random Test Data
+ PseudoCode
+ 1) Establish an alphabet string
+ 2) Create an empty result array
+ 3) Iterate through the integer input n, each time:
+      - Generate a random number i between 1 to 10
+      - Create empty string
+      - Iterate through n, each time:
+          - Randomly choosing a letter from alphabet string and attach it to string
+      - Add string to result array
+ 4) Return result array
+*****************************************************************/
+
+function RandomData(n) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var resultArray = [];
+  for (var i=0; i<n; i++) {
+    var word = "";
+    for (var j=0; j<Math.floor(Math.random()*10)+1; j++) {
+      word += alphabet[Math.floor(Math.random()*26)];
+    }
+    resultArray.push(word);
+  }
+  return resultArray;
+}
+
+/* Driver Code */
+for (var i=0; i<10; i++) {
+  console.log("Trial " + (i+1) + ":");
+  var testArray = RandomData(5);
+  console.log("Test Array: " + testArray);
+  console.log("Result: " + findLongest(testArray));
+  console.log("***********************************")
+}
