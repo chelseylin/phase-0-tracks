de747e90aedc9583ca95a59b531f60da529aece1
cheleylin <linqiaoshu1992@gmail.com>
2016-10-10 01:29:17 -0700

    Release 1: Added and tested keyValueMatch function

--- js/algos.js
+++ js/algos.js
@@ -1,15 +1,15 @@
 /*****************************************************************
  Release 0: Find the Longest Phrase
  PseudoCode
- Set result equals to empty string;
- for each item in the array, compare its length to result
-   - if it is longer, set result equal to the item
-   - else, do nothing
- return result
+ 1) Set result equals to empty string;
+ 2) for each item in the array, compare its length to result
+      - if it is longer, set result equal to the item
+      - else, do nothing
+ 3) return result
 *****************************************************************/
 
 function findLongest(array) {
-  result = "";
+  var result = "";
   for (i = 0; i < array.length; i++) {
     if (array[i].length > result.length) {
       result = array[i];
@@ -20,3 +20,27 @@ function findLongest(array) {
 
 /* Driver Code */
 console.log(findLongest(["long phrase","longest phrase","longer phrase"]));
+
+/*****************************************************************
+ Release 1: Find a Key-Value Match
+ PseudoCode
+ 1) Find all keys in object 1 and save into an array
+ 2) For each keys in  array:
+      - if value in object 1 equals value in object 2, return true
+ 3) return false
+*****************************************************************/
+
+function keyValueMatch(object1, object2) {
+  var keys = Object.keys(object1);
+  for (i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    if (object1[key] == object2[key]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/* Driver Code */
+console.log(keyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+
