68d505d111a79b8f51a451755b06858a0a925f96
cheleylin <linqiaoshu1992@gmail.com>
2016-10-10 00:56:23 -0700

    Added code to create object with name as key and color as value

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,20 @@ colors.push("pink");
 // Add another name: Jack
 names.push("Jack");
 
+// Create Objects
+var horseList = {};
+
+// Check array length
+if (colors.length != names.length) {
+  console.log("Wrong Number of Array Items.");
+} 
+else {
+  for (i = 0; i < colors.length; i++) {
+    horseList[names[i]] = colors[i];
+  }
+}
+
+// Driver Code
+// console.log(horseList);
+// console.log(horseList['Abby']);
+// console.log(horseList.Ed);
